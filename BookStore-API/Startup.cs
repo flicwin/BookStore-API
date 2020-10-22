f76bf9f19a4fb4f2ee8d089a7249f49f0bbcc9ad
flicwin <54873258+flicwin@users.noreply.github.com>
2020-10-22 17:01:51 +1300

    Unit 17 Review and add changes to GitHub

--- BookStore-API/Startup.cs
+++ BookStore-API/Startup.cs
@@ -1,5 +1,7 @@
+using AutoMapper;
 using BookStore_API.Contracts;
 using BookStore_API.Data;
+using BookStore_API.Mappings;
 using BookStore_API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -43,6 +45,9 @@ namespace BookStore_API
                 .AllowAnyMethod()
                 .AllowAnyHeader());
             });
+            
+            services.AddAutoMapper(typeof(Maps));
+
             services.AddSwaggerGen(c => {
                 c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo 
                 { 
